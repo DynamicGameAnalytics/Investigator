223cf205ddd0cbf2a5310010e2b7a26ffa6d9e96
Lukas Höppner <lukas.hoeppner@gmail.com>
2014-11-27 18:38:33 +0100

    draw multiple event types in one graph

--- client/templates/game.js
+++ client/templates/game.js
@@ -20,8 +20,14 @@ Template.createGameForm.events({
 });
 
 Template.game.rendered = function() {
+  var start = moment();
+
+  var palette = new Rickshaw.Color.Palette({
+    scheme: 'colorwheel'
+  });
 
   var rawData = {};
+
   this.data.records.forEach(function(record) {
     var type = record.event.type;
     var time = moment(record.createdAt);
@@ -29,17 +35,35 @@ Template.game.rendered = function() {
     time.seconds(0);
 
     var timeString = time.toISOString();
+    var timeBefore = time.subtract(1, 'm').toISOString();
+    var timeAfter = time.add(2, 'm').toISOString();
     // console.log(timeString);
+    // console.log(timeAfter);
     rawData[type] = rawData[type] || {};
+    rawData[type][timeBefore] = rawData[type][timeBefore] || 0;
     rawData[type][timeString] = rawData[type][timeString] || 0;
+    rawData[type][timeAfter] = rawData[type][timeAfter] || 0;
     ++rawData[type][timeString];
   });
+  // console.log(rawData);
+  for (var i in rawData) {
+    // console.log('i: ' + i);
+    for (var j in rawData) {
+      // console.log('j: ' + j);
+      if (i === j)
+        continue;
+      for (var time in rawData[j]) {
+        rawData[i][time] = rawData[i][time] || 0;
+        // console.log ('set rawData[' + i + '][' + time + '] to ' + rawData[i][time])
+      }
+    }
+  }
 
   var series = new Array();
   for (var type in rawData) {
     var serie = {
       name: type,
-      color: 'steelblue',
+      color: palette.color(),
       data: new Array(),
     };
     for (var time in rawData[type]) {
@@ -48,9 +72,10 @@ Template.game.rendered = function() {
         y: rawData[type][time]
       });
     }
-    serie.data.sort(function(a, b){
+    serie.data = serie.data.sort(function(a, b) {
       return a.x - b.x;
     });
+    // console.log(serie.data);
     series.push(serie);
   }
 
@@ -59,12 +84,29 @@ Template.game.rendered = function() {
   var graph = new Rickshaw.Graph({
     element: document.querySelector('#records-graph'),
     renderer: 'bar',
+    // interpolation: 'linear',
+    // stack: false,
     series: series
   });
 
-  var axes = new Rickshaw.Graph.Axis.Time({
+  var timeAxis = new Rickshaw.Graph.Axis.Time({
     graph: graph
   });
 
+  var countAxis = new Rickshaw.Graph.Axis.Y({
+    graph: graph
+  });
+
+  // var hoverDetail = new Rickshaw.Graph.HoverDetail({
+  //   graph: graph
+  // });
+
+  // var slider = new Rickshaw.Graph.RangeSlider({
+  //   graph: graph,
+  //   element: document.querySelector('#records-slider')
+  // });
+
   graph.render();
+  var end = moment();
+  console.log('graph rendering took ' + end.diff(start) + 'ms');
 };
