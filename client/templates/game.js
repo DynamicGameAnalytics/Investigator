d455107bb4894ab04a4c87a63a4b586efea77815
Lukas Höppner <lukas.hoeppner@gmail.com>
2014-11-29 18:11:31 +0100

    Autoset owner id on game creation, Add auto form

--- client/templates/game.js
+++ client/templates/game.js
@@ -1,22 +1,22 @@
-Template.createGameForm.events({
-  'submit form': function(event) {
-    event.preventDefault();
-    var name = event.target.name.value;
-    if (!Meteor.userId()) {
-      console.log('not signed in');
-      return;
-    }
-    Games.insert({
-      name: name,
-      owner: Meteor.userId()
-    }, function(err) {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Game created');
-    });
-  }
+Template.insertGameForm.events({
+  // 'submit form': function(event) {
+  //   event.preventDefault();
+  //   var name = event.target.name.value;
+  //   if (!Meteor.userId()) {
+  //     console.log('not signed in');
+  //     return;
+  //   }
+  //   Games.insert({
+  //     name: name,
+  //     owner: Meteor.userId()
+  //   }, function(err) {
+  //     if (err) {
+  //       console.log(err);
+  //       return;
+  //     }
+  //     console.log('Game created');
+  //   });
+  // }
 });
 
 function redrawRecordsGraph() {
