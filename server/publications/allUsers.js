a8b4e9abc39d6b5bdbdd051b792b2338fea70b23
grecep <gregor.cepin@gmail.com>
2014-12-10 23:06:32 +0100

    Dont offer current user in autocomplete

--- server/publications/allUsers.js
+++ server/publications/allUsers.js
@@ -1,4 +1,6 @@
-Meteor.publish("allUsers", function () {
-	var allUsers = Meteor.users.find({});	
+Meteor.publish("allUsers", function () {	
+	//var allUsers = Meteor.users.find({});	
+	var allUsers = Meteor.users.find({_id: {$ne:this.userId}});	// dont offer current user in autocomplete
+
     return allUsers;
 });
