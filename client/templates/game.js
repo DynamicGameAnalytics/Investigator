b91052a675c8180a0967f5959db9e2b92a3f7e69
Lukas Höppner <lukas.hoeppner@gmail.com>
2014-12-15 00:45:07 +0100

    use google-chart

--- client/templates/game.js
+++ client/templates/game.js
@@ -1,26 +1,25 @@
-function redrawRecordsGraph() {
+function setChartData() {
   var start = moment();
 
   var dataStart = moment().subtract(1, Session.get('graphs.records.dataInterval'));
   var dataEnd = moment();
-  var records = Records.find({
+
+  var rawData = {};
+
+  rawData[dataStart.toISOString()] = {};
+  rawData[dataEnd.toISOString()] = {};
+
+  var cols = [{
+    type: 'datetime'
+  }];
+  var types = [];
+
+  Records.find({
     createdAt: {
       "$gte": dataStart.toDate(),
       "$lt": dataEnd.toDate()
     }
-  });
-
-  var palette = new Rickshaw.Color.Palette({
-    scheme: 'colorwheel'
-  });
-
-  var rawData = {};
-
-  rawData.timeScaler = {};
-  rawData.timeScaler[dataStart.toISOString()] = 0;
-  rawData.timeScaler[dataEnd.toISOString()] = 0;
-
-  records.forEach(function(record) {
+  }).forEach(function(record) {
     var type = record.event.type;
     var dataUnit = Session.get('graphs.records.dataUnit');
     var time = moment(record.createdAt).startOf(dataUnit);
@@ -28,129 +27,112 @@ function redrawRecordsGraph() {
     var timeString = time.toISOString();
     var timeBefore = time.subtract(1, dataUnit).toISOString();
     var timeAfter = time.add(2, dataUnit).toISOString();
-    // console.log("time: " + timeString);
-    // console.log("after: " + timeAfter);
-    rawData[type] = rawData[type] || {};
-    rawData[type][timeBefore] = rawData[type][timeBefore] || 0;
-    rawData[type][timeString] = rawData[type][timeString] || 0;
-    rawData[type][timeAfter] = rawData[type][timeAfter] || 0;
-    ++rawData[type][timeString];
+    rawData[timeBefore] = rawData[timeBefore] || {};
+    rawData[timeString] = rawData[timeString] || {};
+    rawData[timeAfter] = rawData[timeAfter] || {};
+
+    rawData[timeBefore][type] = rawData[timeBefore][type] || 0;
+    rawData[timeString][type] = rawData[timeString][type] || 0;
+    rawData[timeAfter][type] = rawData[timeAfter][type] || 0;
+    ++rawData[timeString][type];
+    types.push(type);
   });
-  // console.log(rawData);
-  for (var i in rawData) {
-    // console.log('i: ' + i);
-    for (var j in rawData) {
-      // console.log('j: ' + j);
-      if (i === j)
-        continue;
-      for (var time in rawData[j]) {
-        rawData[i][time] = rawData[i][time] || 0;
-        // console.log ('set rawData[' + i + '][' + time + '] to ' + rawData[i][time])
-      }
-    }
+  if (types.length === 0){
+    types.push('no data');
   }
-
-  var series = new Array();
-  for (var type in rawData) {
-    var serie = {
-      name: type,
-      color: palette.color(),
-      data: new Array(),
-    };
-    for (var time in rawData[type]) {
-      serie.data.push({
-        x: moment(time).unix(),
-        y: rawData[type][time]
-      });
-    }
-    serie.data = serie.data.sort(function(a, b) {
-      return a.x - b.x;
-    });
-    // console.log(serie.data);
-    series.push(serie);
-  }
-
-  // console.log(series);
 
   var element = document.querySelector('#records-graph');
-  element.innerHTML = '';
-  var graph = new Rickshaw.Graph({
-    element: element,
-    // renderer: 'bar',
-    interpolation: 'linear',
-    // stack: false,
-    series: series
+
+  _.uniq(types).forEach(function(type) {
+    cols.push({
+      type: 'number',
+      label: type
+    });
   });
+  element.cols = cols;
 
-  var timeAxis = new Rickshaw.Graph.Axis.Time({
-    graph: graph
+  var rows = [];
+
+  _.keys(rawData).forEach(function(datetime) {
+    var row = [moment(datetime).toDate()];
+    _.uniq(types).forEach(function(type) {
+      var value = rawData[datetime][type] || 0;
+      row.push(value);
+    });
+    rows.push(row);
   });
+  element.rows = rows;
 
-  var countAxis = new Rickshaw.Graph.Axis.Y({
-    graph: graph
-  });
-
-  // var hoverDetail = new Rickshaw.Graph.HoverDetail({
-  //   graph: graph
-  // });
-
-  // var slider = new Rickshaw.Graph.RangeSlider({
-  //   graph: graph,
-  //   element: document.querySelector('#records-slider')
-  // });
-
-  graph.render();
   var end = moment();
   console.log('graph rendering took ' + end.diff(start) + 'ms');
 }
-
 
 Template.game.rendered = function() {
   var oldInterval = undefined;
   Session.setDefault('graphs.records.dataUnit', 'm');
   Session.setDefault('graphs.records.dataInterval', 'h');
-  Session.setDefault('graphs.records.updateInterval', 'm');
+  Session.setDefault('graphs.records.updateInterval', 's');
 
-  Tracker.autorun(function(){
-    if (oldInterval){
+  Tracker.autorun(function() {
+    if (oldInterval) {
       Meteor.clearInterval(oldInterval);
     }
     var interval = moment.duration(1, Session.get('graphs.records.updateInterval'));
-    oldInterval = Meteor.setInterval(redrawRecordsGraph, interval.asMilliseconds());
+    oldInterval = Meteor.setInterval(setChartData, interval.asMilliseconds());
   });
 
-  Tracker.autorun(redrawRecordsGraph);
+  Tracker.autorun(setChartData);
+  setChartData();
 };
 
 Template.game.events({
-  "click .toggleIsSharableByLink": function (event, template) {
-    Games.update(this._id, {$set: {isSharableByLink: ! this.isSharableByLink}});
-    if(this.isSharableByLink){
-      Games.update(this._id, {$set: {isPublic: false}});
+  "click .toggleIsSharableByLink": function(event, template) {
+    Games.update(this._id, {
+      $set: {
+        isSharableByLink: !this.isSharableByLink
+      }
+    });
+    if (this.isSharableByLink) {
+      Games.update(this._id, {
+        $set: {
+          isPublic: false
+        }
+      });
       template.find(".toggleIsPublic").checked = false;
     }
     console.log("IsSharableByLink changed");
   },
-  "click .toggleIsPublic": function (event, template) {
-    Games.update(this._id, {$set: {isPublic: ! this.isPublic}});
-    if(! this.isPublic){
-      Games.update(this._id, {$set: {isSharableByLink: true}});
+  "click .toggleIsPublic": function(event, template) {
+    Games.update(this._id, {
+      $set: {
+        isPublic: !this.isPublic
+      }
+    });
+    if (!this.isPublic) {
+      Games.update(this._id, {
+        $set: {
+          isSharableByLink: true
+        }
+      });
       template.find(".toggleIsSharableByLink").checked = true;
     }
     console.log("IsPublic changed");
   },
-  "change #recordsGraphDataUnit": function(event, template){
+  "change #recordsGraphDataUnit": function(event, template) {
     Session.set("graphs.records.dataUnit", event.target.value);
   },
-  "change #recordsGraphUpdateInterval": function(event, template){
+  "change #recordsGraphUpdateInterval": function(event, template) {
     Session.set("graphs.records.updateInterval", event.target.value);
   },
-  "change #recordsGraphDataInterval": function(event, template){
+  "change #recordsGraphDataInterval": function(event, template) {
     Session.set("graphs.records.dataInterval", event.target.value);
   },
-  "click .shareToUser": function(event){
+  "click .shareToUser": function(event) {
     var email = document.getElementById("inputShareToUser").value;
-    if(GameSharedToUser.find({game: this._id, sharedToUser: email}).count()===0 ){
+    if (GameSharedToUser.find({
+        game: this._id,
+        sharedToUser: email
+      }).count() === 0) {
       GameSharedToUser.insert({
         game: this._id,
         sharedToUser: email
@@ -158,7 +140,7 @@ Template.game.events({
     }
     document.getElementById("inputShareToUser").value = "";
   },
-  "click .deleteShare": function(event){
+  "click .deleteShare": function(event) {
     //alert(this._id +" "+ this.sharedToUser);
     GameSharedToUser.remove({
       _id: this._id
@@ -167,29 +149,27 @@ Template.game.events({
 });
 
 Template.game.helpers({
-    isOwner: function () {
-      return this.owner === Meteor.userId();
-    },
-    settings: function() {
-      return {
-        position: "top",
-        limit: 5,
-        rules: [
-        {
-          //token: '@',
-          collection: Meteor.users,
-          field: "publicUsername",
-          //field: "_id",
-          template: Template.userPill
-        },
-        ]
-      }
+  isOwner: function() {
+    return this.owner === Meteor.userId();
+  },
+  settings: function() {
+    return {
+      position: "top",
+      limit: 5,
+      rules: [{
+        //token: '@',
+        collection: Meteor.users,
+        field: "publicUsername",
+        //field: "_id",
+        template: Template.userPill
+      }, ]
     }
+  }
 });
 
 Template.deleteGame.helpers({
-  beforeRemove: function () {
-    return function (collection, id) {
+  beforeRemove: function() {
+    return function(collection, id) {
       var doc = collection.findOne(id);
       if (confirm('Really delete game "' + doc.name + '"?')) {
         this.remove();
